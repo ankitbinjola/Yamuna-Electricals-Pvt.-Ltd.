3d756324402aa93da8e2e99beb3674d5c916eddd
ankit binjola <abinjola.binjola@gmail.com>
2020-05-14 13:41:25 +0530

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+function sendEmail(){
+    Email.send({
+    Host : "smtp.gmail.com",
+    Username : "abinjola.binjola@gmail.com",
+    Password : "Pashupati@21011993",
+    To : 'abinjola.binjola@gmail.com',
+    From : "abinjola.binjola@gmail.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
+}
